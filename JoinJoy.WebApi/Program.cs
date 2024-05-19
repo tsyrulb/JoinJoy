0d022ee893ad06ec3888a91057a0491ed1d85909
tsyrulb <tsyrulnikov.borys@gmail.com>
2024-05-20 00:17:22 +0300

    some changes

--- JoinJoy.WebApi/Program.cs
+++ JoinJoy.WebApi/Program.cs
@@ -1,25 +1,31 @@
+// File: JoinJoy.WebApi/Program.cs
+using Microsoft.EntityFrameworkCore;
+using JoinJoy.Infrastructure;
+using JoinJoy.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<JoinJoyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
