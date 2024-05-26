12044d79cf55992ba2232810b77738b8da662dcb
tsyrulb <tsyrulnikov.borys@gmail.com>
2024-05-27 02:09:18 +0300

    Solved problem with 'https'

--- JoinJoy.WebApi/Startup.cs
+++ JoinJoy.WebApi/Startup.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using JoinJoy.Core.Services;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.OpenApi.Models;
 using JoinJoy.Infrastructure.Data;
+using JoinJoy.Core.Interfaces;
+using JoinJoy.Core.Services;
+using JoinJoy.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using JoinJoy.WebApi.Hubs;
+using JoinJoy.WebApi.Middleware;
 
 namespace JoinJoy.WebApi
 {
@@ -20,11 +20,20 @@ namespace JoinJoy.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add DbContext configuration
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllers();
+            // Register repositories
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IActivityRepository, ActivityRepository>();
+            services.AddScoped<IMatchRepository, MatchRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+            services.AddScoped<IChatMessageRepository, ChatMessageRepository>();
 
+            // Register services
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IActivityService, ActivityService>();
             services.AddScoped<IMatchService, MatchService>();
@@ -32,7 +41,16 @@ namespace JoinJoy.WebApi
             services.AddScoped<IFeedbackService, FeedbackService>();
             services.AddScoped<IChatService, ChatService>();
 
+            services.AddControllers();
+
+            // Add SignalR
             services.AddSignalR();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "JoinJoy API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,13 +67,24 @@ namespace JoinJoy.WebApi
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseMiddleware<JoinJoy.WebApi.Middleware.AuthenticationMiddleware>();
-            app.UseMiddleware<JoinJoy.WebApi.Middleware.ChatMiddleware>();
+            app.UseMiddleware<AuthenticationMiddleware>();
+            app.UseMiddleware<ChatMiddleware>();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JoinJoy API v1");
+                c.RoutePrefix = string.Empty;  // Set Swagger UI at the root of the application
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<JoinJoy.WebApi.Hubs.ChatHub>("/chathub");
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
